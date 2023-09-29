db7b81a2429ce1b1795ab63d70177423e239f50d
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-09-29 13:25:33 +0100

    fixed the filter to return no task found when the list is empty

--- src/Pages/AppContent.js
+++ src/Pages/AppContent.js
@@ -6,28 +6,32 @@ import '../Components/appcontent.css';
 const AppContent = () => {
   const toDoList = useSelector((state) => state.todo.todoList);
 
-  const initialFilterStatus = useSelector((state) => state.todo.filterStatus); 
+  const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
 
-  const sortedTodoList = [...toDoList];
-  sortedTodoList.sort((a, b) => new Date(b.time) - new Date(a.time));
+  // Sort tasks by date in descending order (most recent first)
+  const sortedTodoList = [...toDoList].sort(
+    (a, b) => new Date(b.time).getTime() - new Date(a.time).getTime()
+  );
 
-  const filteredTodoList = sortedTodoList.filter(item => {
-    if (initialFilterStatus === 'all') {
-      return true;
-    }
-    return item.status === initialFilterStatus;
-  });
+  let filteredTodoList = sortedTodoList;
+
+  if (initialFilterStatus === 'complete') {
+    filteredTodoList = sortedTodoList.filter((item) => item.status === 'Complete');
+  } else if (initialFilterStatus === 'incomplete') {
+    filteredTodoList = sortedTodoList.filter((item) => item.status !== 'Complete');
+  }
 
   return (
     <div className='content__wrapper'>
-      {filteredTodoList && filteredTodoList.length > 0
-        ? filteredTodoList.map((todo) => (
+      {filteredTodoList && filteredTodoList.length > 0 ? (
+        filteredTodoList.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))
-        : null}
+      ) : (
+        <p className='notask'>No tasks found.</p>
+      )}
     </div>
   );
 };
 
 export default AppContent;
-
